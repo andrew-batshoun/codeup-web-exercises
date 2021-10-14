f836d480a7b0cd89a4bbef6487f0134ec13f8818
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2021-10-14 10:21:48 -0400

    morning exercise for october 14

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -17,3 +17,17 @@ if(false){
 //It does not run because the condition is not true.
 
 })();
+
+//*******************OCTOBER 14*******************
+
+//The following code will set the variable hadBreakfast to a random boolean value.
+// Copy and paste the following code into warmups JS file:
+
+var hadBreakfast = Boolean(Math.round(Math.random()));
+
+//Write a ternary expression to set a variable of hadBreakfastMessage to a string
+// expressing whether or not you have had breakfast. You will use the variable hadBreakfast as your condition.
+
+var hadBreakfastMessage = (hadBreakfast) ? "I had breakfast today, it was delicious!" : "I didn't have breakfast and I am starving";
+
+console.log(hadBreakfastMessage);
