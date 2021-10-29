1bb2ba8520103965059bbff7f92c3f4db06ca4db
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2021-10-29 18:35:20 -0400

    completed more arrays extra practice, one more left

--- js/extra-exercises-arrays.js
+++ js/extra-exercises-arrays.js
@@ -236,13 +236,7 @@ console.log(getIndexesOf("a", "banana"));
 // // Example: removeAll([1, 2, 3], 2) should return [1, 3]
 // // Example 2: removeAll([2, 2, 3, 4, 5, 2, 2], 2) should return [3, 4, 5]
 
-function removeAll(arr, value){
-    var arrayArray = [];
-    for (var l = 0; l < arr.length; l++ ){
-
-    }
-}
-
+console.log(removeAll([1,2,3], 2));
 // // Exercise 10. Write a function named firstTenFibonacciNumbers() that returns an array of the first ten fibonacci numbers
 function firstTenFibonacciaNumbers(){
     return [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,];
